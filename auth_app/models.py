178037b08bfe94586145e10f600b06fff96650ed
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-05-12 22:02:29 +0200

    feat: implement UserProfile model with user and fullname fields

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fullname = models.CharField(
+        max_length=255
+    )  # the fullname is mandory by the registration
+
+    def __str__(self) -> str:
+        return self.fullname
