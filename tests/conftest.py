0f400e6c44047fc047c91d3c8dd2a9848ac90e8d
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-05-14 22:51:22 +0200

    feat: refactor user registration tests to use mock user data for consistency

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,28 +2,32 @@ import pytest
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 
+mock_user_data = dict(
+    fullname='Example User', email='example@mail.com', password='examplePassword123'
+)
+
 
 @pytest.fixture
 def client():
     client = APIClient()
-    client.raise_request_exception = False  # for checking 500
+    client.raise_request_exception = False  # for Testing 500
     return client
 
 
 @pytest.fixture
 def user():
     return User.objects.create_user(
-        username='example@mail.com',
-        email='example@mail.com',
-        password='examplePassword123',
+        username=mock_user_data['fullname'],
+        email=mock_user_data['email'],
+        password=mock_user_data['password'],
     )
 
 
 @pytest.fixture
 def user_registration_payload():
     return dict(
-        fullname='Example User',
-        email='example@mail.com',
-        password='examplePassword123',
-        repeated_password='examplePassword123',
+        fullname=mock_user_data['fullname'],
+        email=mock_user_data['email'],
+        password=mock_user_data['password'],
+        repeated_password=mock_user_data['password'],
     )
