b7f65c1ab75e3136d63250a95aa5219f5090512a
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-05-14 14:58:51 +0200

    feat: add registration view for user creation and validation

---
+++ auth_app/api/views.py
@@ -0,0 +1,20 @@
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import RegistrationSerializer
+
+
+class RegistrationView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            return Response(user, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
