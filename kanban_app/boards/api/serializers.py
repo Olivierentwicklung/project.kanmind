648fef3a27a83fb9dd9c31b53975a4ebc5698e00
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-05-16 20:50:24 +0200

    feat: implement board creation with member assignment in BoardListSerializer

--- kanban_app/boards/api/serializers.py
+++ kanban_app/boards/api/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
+from auth_app.models import UserProfile
 from kanban_app.boards.models import Board
 
 
 class BoardListSerializer(serializers.ModelSerializer):
+    members = serializers.PrimaryKeyRelatedField(
+        queryset=UserProfile.objects.all(),
+        many=True,
+        write_only=True,
+        allow_empty=False,
+    )
+
     member_count = serializers.IntegerField(read_only=True)
     ticket_count = serializers.IntegerField(read_only=True)
     tasks_to_do_count = serializers.IntegerField(read_only=True)
@@ -21,3 +29,16 @@ class BoardListSerializer(serializers.ModelSerializer):
             'tasks_high_prio_count',
             'owner_id',
         ]
+
+    def create(self, validated_data):
+        members = validated_data.pop('members')
+        owner = self.context['request'].user.userprofile
+
+        board = Board.objects.create(
+            owner=owner,
+            **validated_data,
+        )
+
+        board.members.set(members)
+
+        return board
