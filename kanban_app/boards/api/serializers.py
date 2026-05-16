d75bbdfda406364345a037f9fd2c265bb1772193
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-05-16 15:00:47 +0200

    feat: implement BoardListSerializer for enhanced board data representation

---
+++ kanban_app/boards/api/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from kanban_app.boards.models import Board
+from kanban_app.tasks.models import Task
+
+
+class BoardListSerializer(serializers.ModelSerializer):
+    member_count = serializers.SerializerMethodField()
+    ticket_count = serializers.SerializerMethodField()
+    tasks_to_do_count = serializers.SerializerMethodField()
+    tasks_high_prio_count = serializers.SerializerMethodField()
+    owner_id = serializers.IntegerField(source='owner.id', read_only=True)
+
+    class Meta:
+        model = Board
+        fields = [
+            'id',
+            'title',
+            'member_count',
+            'ticket_count',
+            'tasks_to_do_count',
+            'tasks_high_prio_count',
+            'owner_id',
+        ]
+
+    def get_member_count(self, obj):
+        return obj.members.count()
+
+    def get_ticket_count(self, obj):
+        return obj.tasks.count()
+
+    def get_tasks_to_do_count(self, obj):
+        return obj.tasks.filter(status=Task.Status.TODO).count()
+
+    def get_tasks_high_prio_count(self, obj):
+        return obj.tasks.filter(priority=Task.Priority.HIGH).count()
