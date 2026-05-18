94ca9749f78b59673ce92c32864a4f351896a813
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-05-18 14:36:44 +0200

    feat: add URL routing for reviewing tasks endpoint

--- kanban_app/tasks/api/urls.py
+++ kanban_app/tasks/api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import AssignedTasksView
+from .views import AssignedTasksView, ReviewTasksView
 
 urlpatterns = [
     path(
         'tasks/assigned-to-me/',
         AssignedTasksView.as_view(),
         name='assigned-tasks',
+    ),
+    path(
+        'tasks/reviewing/',
+        ReviewTasksView.as_view(),
+        name='review-tasks',
     ),
 ]
