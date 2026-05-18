3ac85d5dd1fd882c3a294f3e86fa824b3ed44d40
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-05-18 16:46:13 +0200

    feat: implement TaskDetailView for retrieving and updating tasks with appropriate permissions

--- kanban_app/tasks/api/views.py
+++ kanban_app/tasks/api/views.py
@@ -1,14 +1,19 @@
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from kanban_app.boards.models import Board
 from kanban_app.tasks.models import Task
 
-from .serializers import TaskCreateSerializer, TaskSerializer
+from .permissions import IsTaskBoardMemberOrOwner
+from .serializers import TaskCreateSerializer, TaskSerializer, TaskUpdateSerializer
 
 
 class AssignedTasksView(ListAPIView):
@@ -84,6 +89,70 @@ class TaskCreateView(CreateAPIView):
             return Response(
                 response_serializer.data,
                 status=status.HTTP_201_CREATED,
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class TaskDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [
+        IsAuthenticated,
+        IsTaskBoardMemberOrOwner,
+    ]
+
+    lookup_url_kwarg = 'task_id'
+
+    def get_serializer_class(self):  # type:ignore
+        if self.request.method == 'PATCH':
+            return TaskUpdateSerializer
+
+        return TaskSerializer
+
+    def get_queryset(self):  # type:ignore
+        return Task.objects.select_related(
+            'board',
+            'assignee__user',
+            'reviewer__user',
+        ).annotate(
+            comments_count=Count('comments', distinct=True),
+        )
+
+    def partial_update(self, request, *args, **kwargs):
+        task = self.get_object()
+
+        serializer = TaskUpdateSerializer(
+            task,
+            data=request.data,
+            partial=True,
+            context=self.get_serializer_context(),
+        )
+
+        if serializer.is_valid():
+            updated_task = serializer.save()
+
+            response_task = (
+                Task.objects.select_related(
+                    'board',
+                    'assignee__user',
+                    'reviewer__user',
+                )
+                .annotate(
+                    comments_count=Count('comments', distinct=True),
+                )
+                .get(id=updated_task.id)
+            )
+
+            response_serializer = TaskSerializer(
+                response_task,
+                context=self.get_serializer_context(),
+            )
+
+            return Response(
+                response_serializer.data,
+                status=status.HTTP_200_OK,
             )
 
         return Response(
